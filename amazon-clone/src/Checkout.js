0ea34edd5fafa4719ee16acee466f6ccb4ef5d10
djole-dev <djsuboticdev@gmail.com>
2020-11-24 10:39:16 +0100

    Home page
    Created homePage

---
+++ amazon-clone/src/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Checkout.css';
+
+function Checkout() {
+    return (
+        <div className="checkout">
+            This is checkout
+        </div>
+    )
+}
+
+export default Checkout;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyATWB7NIZGngewb8RmaxwtB53HQclWQK4g",
+    authDomain: "clone-662bf.firebaseapp.com",
+    databaseURL: "https://clone-662bf.firebaseio.com",
+    projectId: "clone-662bf",
+    storageBucket: "clone-662bf.appspot.com",
+    messagingSenderId: "349855146464",
+    appId: "1:349855146464:web:49b37327d0c8c18bc34b85",
+    measurementId: "G-M4K9L3LB9F"
+  };
