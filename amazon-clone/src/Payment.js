e432fc44831b15177f461873de9f9b003acedea4
djole-dev <djsuboticdev@gmail.com>
2020-12-01 11:45:43 +0100

    DB firestore
    Google Cloud Firestore setup

--- amazon-clone/src/Payment.js
+++ amazon-clone/src/Payment.js
@@ -7,6 +7,7 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
 import axios from './axios';
+import { db } from "./firebase";
 
 
 function Payment() {
@@ -51,7 +52,15 @@ function Payment() {
         }).then(({ paymentIntent }) => {
             // paymentIntent = payment confirmation
 
-            
+            db.collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .doc(paymentIntent.id)
+            .set({
+                basket:basket,
+                amount: paymentIntent.amount,
+                created:paymentIntent.created
+            })
 
             setSucceeded(true);
             setError(null)
